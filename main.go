097732e5fd5e4db4059b8d051fc90056f3383366
Farhan Sajid <FarhanSajid7861@gmail.com>
2019-07-10 03:00:47 -0400

    mongodb added

--- main.go
+++ main.go
@@ -2,18 +2,34 @@ package main
 
 import (
 	"context"
-	"io/ioutil"
 	"log"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
 	pb "github.com/farhansajid1/go-micro-project/consignment-service/proto"
 	vessel "github.com/farhansajid1/go-micro-project/vessel-service/proto/vessel"
-	"github.com/golang/protobuf/proto"
 	"github.com/micro/go-micro"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type server struct{}
+
+type ConsignmentItem struct {
+	// primitive.ObjectID is the id of the bson object
+	ID          primitive.ObjectID `bson:"_id,omitempty"` // generated on the fly
+	Description string             `bson:"description"`
+	Weight      int32              `bson:"weight"`
+	VesselId    string             `bson:"vessel_id"`
+	Containers   []*pb.Container    `bson:"container"`
+}
+
+/*string id = 1;
+  string description = 2;
+  int32 weight = 3;
+  repeated Container containers = 4;
+  string vessel_id = 5; */
 
 var vesselClient vessel.VesselService
 
@@ -23,6 +39,23 @@ func createVesselReq() *vessel.Specification {
 		Capacity:  500,
 	}
 	return ves
+}
+
+func toBSON(req *pb.Consignment, item *pb.Container) *ConsignmentItem {
+	// convert req to bson object
+	obj := &ConsignmentItem{
+		Description: req.GetDescription(),
+		Weight:      req.GetWeight(),
+		Containers: []*pb.Container{
+			&pb.Container{
+				Id:         item.GetId(),
+				CustomerId: item.GetCustomerId(),
+				Origin:     item.GetOrigin(),
+				UserId:     item.GetUserId(),
+			},
+		},
+	}
+	return obj
 }
 
 func (s *server) CreateConsignment(ctx context.Context, req *pb.Consignment, resp *pb.Response) error {
@@ -38,8 +71,14 @@ func (s *server) CreateConsignment(ctx context.Context, req *pb.Consignment, res
 
 	log.Printf("Found a vessel %v\t%v", vesselresp.Vessel.Descriptor, vesselresp.Vessel.Id)
 
+	obj := toBSON(req, item)
+	result, err := collection.InsertOne(ctx, obj)
+	if err != nil {
+		log.Printf("error adding the value %v", err)
+	}
+	oid, _ := result.InsertedID.(primitive.ObjectID)
 	create := &pb.Consignment{
-		Id:          req.GetId(),
+		Id:          oid.Hex(),
 		Description: req.GetDescription(),
 		Weight:      req.GetWeight(),
 		Containers: []*pb.Container{
@@ -52,32 +91,38 @@ func (s *server) CreateConsignment(ctx context.Context, req *pb.Consignment, res
 		},
 		VesselId: vesselresp.Vessel.Id,
 	}
-	b, err := proto.Marshal(create)
-	if err != nil {
-		log.Printf("could not write to bytes %v", err)
-	}
-	ioutil.WriteFile("./input", b, 0644)
+	// b, err := proto.Marshal(create)
+	// if err != nil {
+	// 	log.Printf("could not write to bytes %v", err)
+	// }
+	// ioutil.WriteFile("./input", b, 0644)
 
 	resp.Created = true
 	resp.Consignment = create
 	return nil
 }
-var client *mongo.Database
+
+var collection *mongo.Collection
 var err error
+
 func main() {
 	service := micro.NewService(
 		micro.Name("go.micro.srv.consignment"),
 		micro.RegisterTTL(time.Second*30),
 		micro.RegisterInterval(time.Second*10),
 	)
+
+	// database
+	log.Printf("Connecting to the database")
+	collection = connectDB()
+	
 	service.Init()
 	pb.RegisterShippingServiceHandler(service.Server(), new(server))
 
 	// Register the client for the vessel.
 	vesselClient = vessel.NewVesselService("go.micro.srv.vessel", service.Client())
 
-	// database
-	client = connectDB()
+
 
 	if err := service.Run(); err != nil {
 		log.Fatal(err)
@@ -85,8 +130,8 @@ func main() {
 
 }
 
-func connectDB(host string) *mongo.Client {
-	client, err = mongo.NewClient(options.Client().ApplyURI("mongodb://@localhost:27017"))
+func connectDB() *mongo.Collection {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://@localhost:27017"))
 	if err != nil {
 		log.Fatalf("failed to create new MongoDB client: %#v", err)
 	}
@@ -99,5 +144,6 @@ func connectDB(host string) *mongo.Client {
 	}
 
 	log.Printf("connected successfully")
-	return client
+	collection = client.Database("consignments").Collection("new")
+	return collection
 }
